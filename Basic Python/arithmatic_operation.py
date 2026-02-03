28d8ca6613542786d3e4e172acda0c2b30bdea0b
Data-by-Rohan <rohanpisal689@gmail.com>
2026-02-03 17:15:53 +0530

    add

---
+++ Basic Python/arithmatic_operation.py
@@ -0,0 +1,12 @@
+a=float(input("Enter first number: "))
+b=float(input("Enter second number: "))
+
+print("Addition is :",a+b)
+print("Subtraction is :",a-b)
+print("Multiplication is :",a*b)
+print("Division is :",a/b)
+print("Floor Division is :",a//b)
+print("Modulus is :",a%b)
+print("Exponentiation is :",a**b)   
+
+
